b73049d956f0b01def44e4d5a46c4c4e2b2858a9
AdrianM0 <adrianmirza81@gmail.com>
2023-08-23 11:23:22 +0200

    added args related files.

---
+++ src/molformer/utils.py
@@ -0,0 +1,18 @@
+from argparse import Namespace
+import yaml
+
+
+def read_config(hparams):
+
+    with open(hparams) as f:
+        config = Namespace(**yaml.safe_load(f))
+
+    return config
+
+
+def get_argparse_defaults(parser):
+    defaults = {}
+    for action in parser._actions:
+        if not action.required and action.dest != "help":
+            defaults[action.dest] = action.default
+    return defaults
