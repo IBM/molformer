9963c2725ee77fbb04448170f248a1064ca83788
AdrianM0 <adrianmirza81@gmail.com>
2023-08-23 23:55:36 +0200

    added setup.py, changed molformer to src.molformer

--- src/molformer/analysis.py
+++ src/molformer/analysis.py
@@ -1,10 +1,9 @@
-from molformer.utils import get_argparse_defaults, read_config
-from molformer.model.args import get_parser
-from molformer.tokenizers import MolTranBertTokenizer
-from molformer.model.modeling_bert import get_bert
-from molformer.model.args import parse_args as ARGS
+from src.molformer.utils import get_argparse_defaults, read_config
+from src.molformer.model.args import get_parser
+from src.molformer.tokenizer import MolTranBertTokenizer
+from src.molformer.model.base_bert import get_bert
+from src.molformer.model.args import parse_args as ARGS
 from argparse import AttributeDict
-from molformer.data import bert_vocab
 
 # build|pipeline
 
@@ -20,7 +19,7 @@ CONFIG = read_config(CONFIG_FILE_PATH)
 #?  config.mask = False
 
 ### tokenizer
-VOCAB_PATH = ''
+VOCAB_PATH = 'data/bert_vocab.txt'
 TOKENIZER = MolTranBertTokenizer(VOCAB_PATH)
 
 ### import|model
