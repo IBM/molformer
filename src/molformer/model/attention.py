03735584707164da3ae641e4dc812ee96f90d523
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2023-08-24 19:43:32 +0200

    continue cleanup

--- src/molformer/model/attention.py
+++ src/molformer/model/attention.py
@@ -1,40 +1,30 @@
-from math import sqrt
 from molformer.model.base_bert import LM_Layer
 from molformer.model.attention_modules.rotate_builder import (
     RotateEncoderBuilder as rotate_builder,
 )
-from fast_transformers.events import EventDispatcher, AttentionEvent
-from fast_transformers.attention_registry import (
-    AttentionRegistry,
-    Optional,
-    Float,
-    EventDispatcherInstance,
-)
 from fast_transformers.builders import TransformerEncoderBuilder
 import torch
 from torch import nn
-from torch.nn import Dropout, Module
 
 from fast_transformers.feature_maps import GeneralizedRandomFeatures
 from functools import partial
 from fast_transformers.masking import LengthMask
+from argparse import Namespace
 
 
 class TestBert(nn.Module):
     def __init__(
         self,
-        config,
         vocab,
         model_path=None,
-        extend_pos=False,
         rotate=False,
         device="cpu",
     ):
         if model_path is None:
             assert False
         super().__init__()
-        torch.load(model_path, map_location=torch.device(device))
-        #         config = Namespace(**model["hyper_parameters"])
+        model = torch.load(model_path, map_location=torch.device(device))
+        config = Namespace(**model["hyper_parameters"])
         config.rotate = rotate
         (
             self.tok_emb,
@@ -43,12 +33,6 @@ class TestBert(nn.Module):
             self.drop,
             self.lang_model,
         ) = self.create_model(config, vocab)
-        """if self.pos_emb != None:
-            pos_emb = nn.Parameter(model['state_dict']['pos_emb'])
-
-        if extend_pos is True and self.pos_emb != None:
-            pos_extend = nn.Parameter(torch.zeros(1, 1000, config.n_embd)).to(pos_emb.device)
-            self.pos_emb = nn.Parameter(torch.cat([pos_emb.data, pos_extend.data], dim=1))"""
 
         self.drop = nn.Dropout(config.d_dropout)
 
@@ -78,7 +62,7 @@ class TestBert(nn.Module):
                 query_dimensions=config.n_embd // config.n_head,
                 value_dimensions=config.n_embd // config.n_head,
                 feed_forward_dimensions=config.n_embd,
-                attention_type="linear",
+                attention_type="linearwweights",
                 feature_map=partial(
                     GeneralizedRandomFeatures,
                     n_dims=config.num_feats,
@@ -92,12 +76,11 @@ class TestBert(nn.Module):
 
         blocks = builder.get()
         lang_model = LM_Layer(config.n_embd, n_vocab)
-        block_size = block_size
 
         return tok_emb, pos_emb, blocks, drop, lang_model
 
     def forward(self, batch, mask=None, mode="cls"):
-        b, t = batch.size()
+        _batch, t = batch.size()
 
         # forward the GPT model
         token_embeddings = self.tok_emb(
@@ -141,117 +124,3 @@ class TestBert(nn.Module):
             )  # sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
             sum_mask = input_mask_expanded.sum(1)
             return sum_embeddings / sum_mask, attention_mask
-
-
-class LinearWWeight(Module):
-    """
-    Slightly modify the fast transformers linear attention to return
-    the 'attention weights' for visual analysis
-    """
-
-    def __init__(
-        self, query_dimensions, feature_map=None, eps=1e-6, event_dispatcher=""
-    ):
-        super(LinearWWeight, self).__init__()
-
-        self.feature_map = feature_map(query_dimensions)
-        self.eps = eps
-        self.event_dispatcher = EventDispatcher.get(event_dispatcher)
-
-    def forward(self, queries, keys, values, attn_mask, query_lengths, key_lengths):
-        # Apply the feature map to the queries and keys
-        self.feature_map.new_feature_map(queries.device)
-        Q = self.feature_map.forward_queries(queries)
-        K = self.feature_map.forward_keys(keys)
-
-        # Apply the key padding mask and make sure that the attn_mask is
-        # all_ones
-        if not attn_mask.all_ones:
-            raise RuntimeError(
-                ("LinearAttention does not support arbitrary " "attention masks")
-            )
-        K = K * key_lengths.float_matrix[:, :, None, None]
-
-        # Compute the KV matrix, namely the dot product of keys and values so
-        # that we never explicitly compute the attention matrix and thus
-        # decrease the complexity
-        KV = torch.einsum("nshd,nshm->nhmd", K, values)
-
-        # Compute the normalizer
-        Z = 1 / (torch.einsum("nlhd,nhd->nlh", Q, K.sum(dim=1)) + self.eps)
-
-        attention = torch.einsum("nlhd, nshd->nlsh", Q, K)
-        # try both postive and negative values
-
-        attention_norm = 1 / (torch.einsum("nlsh->nlh", attention + self.eps))
-        attention_out = torch.einsum("nlsh, nlh->nlsh", attention, attention_norm)
-        attention_out = torch.einsum("nlsh->nhls", attention_out)
-        # remove negative numbers with relu
-
-        # Finally compute and return the new values
-        V = torch.einsum("nlhd,nhmd,nlh->nlhm", Q, KV, Z)
-
-        return V.contiguous(), attention_out.detach()
-
-
-class FullWWeight(Module):
-    """
-    Slightly modify the fast transformers Full attention to return
-    the 'attention weights' for visual analysis
-    """
-
-    def __init__(self, softmax_temp=None, attention_dropout=0.1, event_dispatcher=""):
-        super(FullWWeight, self).__init__()
-
-        self.softmax_temp = softmax_temp
-        self.dropout = Dropout(attention_dropout)
-        self.event_dispatcher = EventDispatcher.get(event_dispatcher)
-
-    def forward(self, queries, keys, values, attn_mask, query_lengths, key_lengths):
-        # Extract some shapes and compute the temperature
-        N, L, H, E = queries.shape
-        _, S, _, D = values.shape
-        softmax_temp = self.softmax_temp or 1.0 / sqrt(E)
-
-        # Scale the queries instead of applying the softmax temperature to the
-        # dot products
-        queries = queries * softmax_temp
-
-        # Compute the unnormalized attention and apply the masks
-        QK = torch.einsum("nlhe,nshe->nhls", queries, keys)
-        if not attn_mask.all_ones:
-            QK = QK + attn_mask.additive_matrix
-        if not key_lengths.all_ones:
-            QK = QK + key_lengths.additive_matrix[:, None, None]
-
-        # Compute the attention and the weighted average
-        attention_weights = torch.softmax(QK, dim=-1)
-        # A = self.dropout(torch.softmax(QK, dim=-1))
-        A = self.dropout(attention_weights)
-        V = torch.einsum("nhls,nshd->nlhd", A, values)
-
-        # Let the world know of the attention matrix
-        self.event_dispatcher.dispatch(AttentionEvent(self, A))
-
-        # Make sure that what we return is contiguous
-        return V.contiguous(), attention_weights.detach()
-
-
-def register_attention(attention_type="linear"):
-    # Register the attention implementation so that it becomes available in our
-    # builders
-    if attention_type == "linear":
-        weights = FullWWeight
-
-    else:
-        weights = LinearWWeight
-
-    AttentionRegistry.register(
-        f"{attention_type}wweights",
-        weights,
-        [
-            ("softmax_temp", Optional(Float)),
-            ("attention_dropout", Optional(Float, 0.1)),
-            ("event_dispatcher", Optional(EventDispatcherInstance, "")),
-        ],
-    )
