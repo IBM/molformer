89ff6174a29d3a94fe149ce41b2902639af19105
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2023-08-24 18:47:18 +0200

    chore: cleanup

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='molformer',
-    version='0.1.0',
-    packages=find_packages(include=['src/molformer', 'src/molformer.*']),
-    url='https://github.com/lamalab-org/molformer',
-    license='MIT',
-    author='Kevin Maik Jablonka and Adrian Mirza',
-    author_email='',
-    description='Refactored MolFormer',
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
-    package_data={'src/molformer': ['data']},
-    include_package_data=True
+    name="molformer",
+    version="0.1.0",
+    packages=find_packages(
+        where="src",
+        include=["molformer"],
+    ),
+    package_dir={"": "src"},
+    url="https://github.com/lamalab-org/molformer",
+    license="MIT",
+    author="Adrian Mirza and Kevin Maik Jablonka",
+    author_email="",
+    description="Refactored MolFormer",
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
     install_requires=[
-        'transformers==4.6.0',
-        'pytorch-lightning==1.1.5',
-        'pytorch-fast-transformers==0.4.0',
-        'datasets==1.6.2',
-        'jupyterlab==3.4.0',
-        'ipywidgets==7.7.0',
-        'bertviz==1.4.0'
-    ]
-    )
+        "transformers==4.6.0",
+        "pytorch-lightning==1.1.5",
+        "pytorch-fast-transformers==0.4.0",
+        "datasets==1.6.2",
+        "jupyterlab==3.4.0",
+        "ipywidgets==7.7.0",
+        "bertviz==1.4.0",
+    ],
+)
--- src/molformer/model/attention_layer.py
+++ src/molformer/model/attention_layer.py
@@ -6,71 +6,85 @@ attention module.
 
 """
 
-from torch.nn import Linear, Module
 import torch
 from fast_transformers.attention import AttentionLayer
-from fast_transformers.events import EventDispatcher, QKVEvent
+from fast_transformers.events import QKVEvent
 
 
 class RotaryEmbedding(torch.nn.Module):
-    
     def __init__(self, dim, base=10000):
         super().__init__()
-        inv_freq = 1. / (base ** (torch.arange(0, dim, 2).float() / dim))
-        self.register_buffer('inv_freq', inv_freq)
-        self.seq_len_cached = 0 
+        inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).float() / dim))
+        self.register_buffer("inv_freq", inv_freq)
+        self.seq_len_cached = 0
         self.cos_cached = None
         self.sin_cached = None
 
     def forward(self, x, seq_dim=1):
         seq_len = x.shape[seq_dim]
         if seq_len != self.seq_len_cached:
-            #if seq_len > self.seq_len_cached:
+            # if seq_len > self.seq_len_cached:
             self.seq_len_cached = seq_len
             t = torch.arange(x.shape[seq_dim], device=x.device).type_as(self.inv_freq)
-            freqs = torch.einsum('i,j->ij', t, self.inv_freq)
+            freqs = torch.einsum("i,j->ij", t, self.inv_freq)
             emb = torch.cat((freqs, freqs), dim=-1).to(x.device)
-            self.cos_cached = emb.cos()[None,:, None, :]
-            self.sin_cached = emb.sin()[None,:, None, :]
-            #else:
+            self.cos_cached = emb.cos()[None, :, None, :]
+            self.sin_cached = emb.sin()[None, :, None, :]
+            # else:
             #    cos_return = self.cos_cached[..., :seq_len]
             #    sin_return = self.sin_cached[..., :seq_len]
             #    return cos_return, sin_return
-                
+
         return self.cos_cached, self.sin_cached
 
 
 # rotary pos emb helpers:
 
+
 def rotate_half(x):
-    x1, x2 = x[..., :x.shape[-1] // 2], x[..., x.shape[-1] // 2:]
-    return torch.cat((-x2, x1), dim=x1.ndim - 1) # dim=-1 triggers a bug in earlier torch versions
+    x1, x2 = x[..., : x.shape[-1] // 2], x[..., x.shape[-1] // 2 :]
+    return torch.cat(
+        (-x2, x1), dim=x1.ndim - 1
+    )  # dim=-1 triggers a bug in earlier torch versions
+
 
 @torch.jit.script
 def apply_rotary_pos_emb(q, k, cos, sin):
     return (q * cos) + (rotate_half(q) * sin), (k * cos) + (rotate_half(k) * sin)
 
 
-
 class RotateAttentionLayer(AttentionLayer):
-    """Rotate attention layer inherits from fast_transformer attention layer. 
-        The only thing added is an Embedding encoding, for more information
-        on the attention layer see the fast_transformers code
+    """Rotate attention layer inherits from fast_transformer attention layer.
+    The only thing added is an Embedding encoding, for more information
+    on the attention layer see the fast_transformers code
     """
-    def __init__(self, attention, d_model, n_heads, d_keys=None,
-                 d_values=None, event_dispatcher=""):
-        super(RotateAttentionLayer, self).__init__(attention,d_model, n_heads, d_keys=d_keys,
-                 d_values=d_values, event_dispatcher=event_dispatcher)
+
+    def __init__(
+        self,
+        attention,
+        d_model,
+        n_heads,
+        d_keys=None,
+        d_values=None,
+        event_dispatcher="",
+    ):
+        super(RotateAttentionLayer, self).__init__(
+            attention,
+            d_model,
+            n_heads,
+            d_keys=d_keys,
+            d_values=d_values,
+            event_dispatcher=event_dispatcher,
+        )
 
         self.rotaryemb = RotaryEmbedding(d_keys)
-        print('Using Rotation Embedding')
+        print("Using Rotation Embedding")
 
-    def forward(self, queries, keys, values, attn_mask, query_lengths,
-                key_lengths):
+    def forward(self, queries, keys, values, attn_mask, query_lengths, key_lengths):
         """
         Using the same frame work as the fast_Transformers attention layer
         but injecting rotary information to the queries and the keys
-        after the keys and queries are projected. 
+        after the keys and queries are projected.
         In the argument description we make use of the following sizes
 
             - N: the batch size
@@ -110,15 +124,9 @@ class RotateAttentionLayer(AttentionLayer):
         # Let the world know of the qkv
         self.event_dispatcher.dispatch(QKVEvent(self, queries, keys, values))
 
-
         # Compute the attention
         new_values = self.inner_attention(
-            queries,
-            keys,
-            values,
-            attn_mask,
-            query_lengths,
-            key_lengths
+            queries, keys, values, attn_mask, query_lengths, key_lengths
         ).view(N, L, -1)
 
         # Project the output and return
